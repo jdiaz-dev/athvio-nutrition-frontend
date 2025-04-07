0d5b7365a2b44a4f566ea92fd094562f48657f4f
polsito <lordgelsin26@gmail.com>
2025-04-06 19:35:12 -0500

    fix

--- src/modules/patients/patient-console/patient-sidebar/components/PatientSidebar.tsx
+++ src/modules/patients/patient-console/patient-sidebar/components/PatientSidebar.tsx
@@ -1,8 +1,7 @@
-import { useContext, useEffect, useMemo } from 'react';
+import { useContext, useMemo } from 'react';
 
 // material-ui
-import { useTheme } from '@mui/material/styles';
-import { Box, Drawer, useMediaQuery } from '@mui/material';
+import { Box } from '@mui/material';
 
 // project import
 import PatientAvatar from './PatientAvatar/PatientAvatar';
@@ -11,7 +10,6 @@ import PatientDrawerStyled from './PatientDrawerStyled';
 
 //todo: move config?
 import { SidebarContext } from 'src/modules/patients/patient-console/patient-sidebar/context/SidebarContext';
-import { ThemeEnum } from 'src/shared/Consts';
 
 // ==============================|| MAIN LAYOUT - DRAWER ||============================== //
 
@@ -20,57 +18,25 @@ interface Props {
 }
 
 const PatientSidebar = ({ window }: Props) => {
-  const theme = useTheme();
-  const { openSidebar, setOpenSidebar } = useContext(SidebarContext);
-  const matchDownMD = useMediaQuery(theme.breakpoints.down('lg'));
+  const { openSidebar } = useContext(SidebarContext);
 
-  // responsive drawer container
-  const container = window !== undefined ? () => window().document.body : undefined;
-
-  // header content
   const patientAvatar = useMemo(() => <PatientAvatar open={openSidebar} />, [openSidebar]);
   const drawerContent = useMemo(() => <DrawerContent />, []);
 
   return (
     <Box component="nav" sx={{ flexShrink: { md: 0 }, zIndex: 1200 }} aria-label="mailbox folders">
-      {!matchDownMD ? (
-        <PatientDrawerStyled
-          sx={{
-            '& .MuiDrawer-paper': {
-              position: 'absolute',
-            },
-          }}
-          variant="permanent"
-          open={openSidebar}
-        >
-          {patientAvatar}
-          {drawerContent}
-        </PatientDrawerStyled>
-      ) : (
-        <Drawer
-          container={container}
-          variant="permanent"
-          open={openSidebar}
-          onClose={() => {
-            setOpenSidebar(!openSidebar);
-          }}
-          ModalProps={{ keepMounted: true }}
-          sx={{
-            'display': { xs: 'block', lg: 'none' },
-            '& .MuiDrawer-paper': {
-              boxSizing: 'border-box',
-              width: ThemeEnum.DRAWER_WIDTH,
-              borderRight: `1px solid ${theme.palette.divider}`,
-              backgroundImage: 'none',
-              boxShadow: 'inherit',
-              position: 'relative',
-            },
-          }}
-        >
-          {patientAvatar}
-          {drawerContent}
-        </Drawer>
-      )}
+      <PatientDrawerStyled
+        sx={{
+          '& .MuiDrawer-paper': {
+            position: 'absolute',
+          },
+        }}
+        variant="permanent"
+        open={openSidebar}
+      >
+        {patientAvatar}
+        {drawerContent}
+      </PatientDrawerStyled>
     </Box>
   );
 };
