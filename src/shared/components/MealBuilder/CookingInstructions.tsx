83d734e5531f170c72463e23ae6a70a4f6026154
polsito <lordgelsin26@gmail.com>
2025-05-14 17:44:21 -0500

    fix

--- src/shared/components/MealBuilder/CookingInstructions.tsx
+++ src/shared/components/MealBuilder/CookingInstructions.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import { Box, TextField } from '@mui/material';
 import { useDispatch } from 'react-redux';
 import { CurrentModuleContext } from 'src/shared/context/CurrentModuleContext';
@@ -6,11 +6,13 @@ import { useMealBuilderSlicers } from 'src/shared/hooks/useMealBuilderSlicers';
 import { EnableEditionContext } from 'src/shared/components/wrappers/EnablerEditionWrapper/EnableEditionContext';
 import { Modules } from 'src/shared/Consts';
 import { useTranslation } from 'react-i18next';
+import { debounceTime, Subject } from 'rxjs';
 
 function CookingInstructions({ cookingInstructions }: { cookingInstructions: string }) {
   const currentModuleContext = useContext(CurrentModuleContext);
   const enableEditionContext = useContext(EnableEditionContext);
   const { t } = useTranslation();
+  const instructionSubject$ = useRef(new Subject<string>());
 
   const { renameCookingInstruction } = useMealBuilderSlicers(currentModuleContext.currentModule);
   const dispatch = useDispatch();
@@ -19,6 +21,15 @@ function CookingInstructions({ cookingInstructions }: { cookingInstructions: str
     currentModuleContext.currentModule === Modules.CLIENT_PLANS ||
     (currentModuleContext.currentModule === Modules.NUTRITIONAL_MEALS && enableEditionContext.enableEdition);
 
+  useEffect(() => {
+    const subscription = instructionSubject$.current.pipe(debounceTime(300)).subscribe((value) => {
+      dispatch(renameCookingInstruction(value));
+    });
+
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, []);
   return (
     <>
       <Box
@@ -29,25 +40,25 @@ function CookingInstructions({ cookingInstructions }: { cookingInstructions: str
         noValidate
         autoComplete="off"
       >
-        <div>
-          <TextField
-            id="outlined-textarea"
-            label={t('mealBuilder.titles.cookingInstructions')}
-            multiline
-            fullWidth
-            defaultValue={cookingInstructions}
-            onChange={(e) => {
-              dispatch(renameCookingInstruction(e.target.value));
-            }}
-            disabled={!isDisabled}
-            sx={{
-              '& .MuiOutlinedInput-input.Mui-disabled': {
-                WebkitTextFillColor: '#b7afaf !important',
-                cursor: 'not-allowed',
-              },
-            }}
-          />
-        </div>
+        <TextField
+          id="outlined-textarea"
+          label={t('mealBuilder.titles.cookingInstructions')}
+          multiline
+          fullWidth
+          defaultValue={cookingInstructions}
+          onChange={(e) => {
+            const newValue = e.target.value;
+            instructionSubject$.current.next(newValue);
+          }}
+
+          disabled={!isDisabled}
+          sx={{
+            '& .MuiOutlinedInput-input.Mui-disabled': {
+              WebkitTextFillColor: '#b7afaf !important',
+              cursor: 'not-allowed',
+            },
+          }}
+        />
       </Box>
     </>
   );
