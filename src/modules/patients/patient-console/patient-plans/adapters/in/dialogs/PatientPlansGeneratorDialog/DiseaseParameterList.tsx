7b34c8bff871c4766b3dd6e2e4d7cd1e6c905422
jonathanDiaz <lordgelsin26@gmail.com>
2026-02-09 20:47:19 -0500

    fix

--- src/modules/patients/patient-console/patient-plans/adapters/in/dialogs/PatientPlansGeneratorDialog/DiseaseParameterList.tsx
+++ src/modules/patients/patient-console/patient-plans/adapters/in/dialogs/PatientPlansGeneratorDialog/DiseaseParameterList.tsx
@@ -5,7 +5,43 @@ import * as nutritionBuilderSlice from 'src/modules/nutrition-builder/adapters/i
 import { useSelector } from 'react-redux';
 import { ReduxStates } from 'src/shared/types/types';
 import { Card, FormLabel } from '@mui/material';
-import CheckboxController from 'src/modules/patients/patient-console/patient-plans/adapters/in/dialogs/PatientPlansGeneratorDialog/CheckboxController';
+
+import { ChangeEvent, useState } from 'react';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import { useDispatch } from 'react-redux';
+import { AnyAction } from 'redux';
+import { NutriBuilderParamStatus } from 'src/shared/Consts';
+
+type ParamStatus = { uuid: string; status: NutriBuilderParamStatus };
+function CheckboxController({
+  item,
+  reducer,
+}: {
+  item: { uuid: string; spanishName: string };
+  reducer: (param: ParamStatus) => AnyAction;
+}) {
+  const dispatch = useDispatch();
+  const [checked, setChecked] = useState(false);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.checked) {
+      dispatch(reducer({ uuid: event.target.defaultValue, status: NutriBuilderParamStatus.SELECTED }));
+    } else {
+      dispatch(reducer({ uuid: event.target.defaultValue, status: NutriBuilderParamStatus.UNSELECTED }));
+    }
+    setChecked(event.target.checked);
+  };
+
+  return (
+    <FormControlLabel
+      style={{ width: '13%' }}
+      control={<Checkbox checked={checked} onChange={handleChange} />}
+      label={item.spanishName}
+      value={item.uuid}
+    />
+  );
+}
 
 function FormControlContainer({ children }: { children: ReactNode }) {
   return <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'left', border: '1px solid green' }}>{children}</div>;
