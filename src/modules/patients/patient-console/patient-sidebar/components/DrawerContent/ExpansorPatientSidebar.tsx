2e1e84bef39984331850df2d1643b6be9f693823
Jonathan Díaz <jonathan.a.diaz.m@gmail.com>
2026-05-08 20:34:01 -0500

    fix

--- src/modules/patients/patient-console/patient-sidebar/components/DrawerContent/ExpansorPatientSidebar.tsx
+++ src/modules/patients/patient-console/patient-sidebar/components/DrawerContent/ExpansorPatientSidebar.tsx
@@ -7,6 +7,7 @@ import MenuFoldOutlined from '@ant-design/icons/MenuFoldOutlined';
 import { SidebarContext } from 'src/modules/patients/patient-console/patient-sidebar/context/SidebarContext';
 import IconButton from 'src/shared/components/IconButton';
 import useConfig from 'src/shared/hooks/useConfig';
+import { Tooltip } from '@mui/material';
 
 function ExpansorPatientSidebar() {
   const { openSidebar, setOpenSidebar } = useContext(SidebarContext);
@@ -17,17 +18,19 @@ function ExpansorPatientSidebar() {
 
   const iconBackColor = mode === ThemeMode.DARK ? 'background.default' : 'grey.100';
   return (
-    <IconButton
-      aria-label="open drawer"
-      onClick={() => setOpenSidebar(!openSidebar)}
-      edge="start"
-      color="secondary"
-      variant="light"
-      sx={{ color: 'text.primary', bgcolor: openSidebar ? 'transparent' : iconBackColor, ml: { xs: 0, lg: -2 } }}
-      style={{ marginLeft: '9px' }}
-    >
-      {!openSidebar ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-    </IconButton>
+    <Tooltip title={openSidebar ? 'Contraer' : 'Expandir'} placement="right">
+      <IconButton
+        aria-label="open drawer"
+        onClick={() => setOpenSidebar(!openSidebar)}
+        edge="start"
+        color="secondary"
+        variant="light"
+        sx={{ color: 'text.primary', bgcolor: openSidebar ? 'transparent' : iconBackColor, ml: { xs: 0, lg: -2 } }}
+        style={{ marginLeft: '9px' }}
+      >
+        {!openSidebar ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+      </IconButton>
+    </Tooltip>
   );
 }
 
